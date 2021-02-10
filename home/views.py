c12cf81066520108bcfd6832f9428b47371eb523
sadagatasgarov2 <sadagat.asgarov@gmail.com>
2021-02-10 17:47:17 +0400

    sayfalari olusturmaq hakimizda contakt referans ve ana menu

--- home/views.py
+++ home/views.py
@@ -11,3 +11,20 @@ def index(request):
     dept = "Computer Engineering"
     context = {'setting': setting}
     return render(request, 'index.html', context)
+
+def hakkimizda(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page':'hakkimizda'}
+    return render(request, 'hakkimizda.html', context)
+
+
+def referanslar(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page':'hakkimizda'}
+    return render(request, 'referanslar.html', context)
+
+
+def iletisim(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting, 'page':'hakkimizda'}
+    return render(request, 'iletisim.html', context)
