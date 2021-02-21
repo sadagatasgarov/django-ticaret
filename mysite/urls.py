e443904b7ae1060980397fe34f4d7a147840a7a8
sadagatasgarov2 <sadagat.asgarov@gmail.com>
2021-02-22 00:33:54 +0400

    ders26 uye paneluolustrmaq user app eser model ve userpanel user profile

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,16 +23,18 @@ from home import views
 
 urlpatterns = [
                   path('', include('home.urls')),
+                  path('home/', include('home.urls')),
+                  path('product/', include('product.urls')),
+                  path('user/', include('user.urls')),
+                  path('admin/', admin.site.urls),
+                  path('ckeditor/', include('ckeditor_uploader.urls')),
                   path('hakkimizda/', views.hakkimizda, name='hakkimizda'),
                   path('referanslar/', views.referanslar, name='referanslar'),
                   path('iletisim/', views.iletisim, name='iletisim'),
+
                   path('category/<int:id>/<slug:slug>/', views.category_products, name='category_products'),
-                  path('home/', include('home.urls')),
-                  path('home/', include('home.urls')),
-                  path('product/', include('product.urls')),
                   path('product/<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
-                  path('admin/', admin.site.urls),
-                  path('ckeditor/', include('ckeditor_uploader.urls')),
+
                   path('search/', views.product_search, name='product_search'),
                   path('search_auto/', views.product_search_auto, name='product_search_auto'),
                   path('logout/', views.logout_view, name='logout_view'),
